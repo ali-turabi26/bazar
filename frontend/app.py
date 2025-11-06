c562805bbd92a2e741346c6a67901783a27ebbb5
ali-turabi26 <151427117+ali-turabi26@users.noreply.github.com>
2025-11-06 11:49:45 +0200

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -1,25 +1,24 @@
 from flask import Flask, jsonify
 import os, requests
 
-CATALOG_URL = os.environ.get("CATALOG_URL", "http://catalog:5000")
-ORDER_URL   = os.environ.get("ORDER_URL",   "http://order:5000")
-
 app = Flask(__name__)
+CATALOG_URL = os.getenv("CATALOG_URL", "http://catalog:5000")
+ORDER_URL = os.getenv("ORDER_URL", "http://order:5000")
 
 @app.get("/search/<topic>")
 def search(topic):
-    r = requests.get(f"{CATALOG_URL}/search/{topic}", timeout=3)
-    return (r.text, r.status_code, {"Content-Type":"application/json"})
+    r = requests.get(f"{CATALOG_URL}/search/{topic}")
+    return jsonify(r.json())
 
-@app.get("/info/<int:item_id>")
-def info(item_id):
-    r = requests.get(f"{CATALOG_URL}/info/{item_id}", timeout=3)
-    return (r.text, r.status_code, {"Content-Type":"application/json"})
+@app.get("/info/<int:book_id>")
+def info(book_id):
+    r = requests.get(f"{CATALOG_URL}/info/{book_id}")
+    return jsonify(r.json())
 
-@app.post("/purchase/<int:item_id>")
-def purchase(item_id):
-    r = requests.post(f"{ORDER_URL}/purchase/{item_id}", timeout=5)
-    return (r.text, r.status_code, {"Content-Type":"application/json"})
+@app.post("/purchase/<int:book_id>")
+def purchase(book_id):
+    r = requests.post(f"{ORDER_URL}/purchase/{book_id}")
+    return jsonify(r.json())
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
